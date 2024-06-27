a068dca32e2d9a56051dc03d4edaa672dae91f7a
Priyanshu-choudhary <broyadi23@gmail.com>
2024-06-27 08:19:19 +0530

    use radis cloud

--- src/main/java/com/example/WebSecurityExample/ConfigSecurity/RedisCacheConfig.java
+++ src/main/java/com/example/WebSecurityExample/ConfigSecurity/RedisCacheConfig.java
@@ -6,16 +6,38 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.cache.RedisCacheConfiguration;
 import org.springframework.data.redis.cache.RedisCacheManager;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.lettuce.LettuceClientConfiguration;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.RedisSerializationContext;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
-
-import java.time.Duration;
+import io.lettuce.core.ClientOptions;
+import io.lettuce.core.resource.ClientResources;
+import io.lettuce.core.resource.DefaultClientResources;
 
 @Configuration
 @EnableCaching
 public class RedisCacheConfig {
+
+    @Bean(destroyMethod = "shutdown")
+    public ClientResources clientResources() {
+        return DefaultClientResources.create();
+    }
+
+    @Bean
+    public RedisConnectionFactory redisConnectionFactory(ClientResources clientResources) {
+        RedisStandaloneConfiguration redisConfig = new RedisStandaloneConfiguration("redis-10304.c294.ap-northeast-1-2.ec2.redns.redis-cloud.com", 10304);
+        redisConfig.setPassword("2WK6HIaXQedIS8uQbjDzRlSu00B9AwDL");
+
+        LettuceClientConfiguration clientConfig = LettuceClientConfiguration.builder()
+                .clientResources(clientResources)
+//                .clientOptions(ClientOptions.builder().ssl(true).build())
+                .build();
+
+        return new LettuceConnectionFactory(redisConfig, clientConfig);
+    }
 
     @Bean
     public RedisTemplate<Object, Object> redisTemplate(RedisConnectionFactory connectionFactory) {
