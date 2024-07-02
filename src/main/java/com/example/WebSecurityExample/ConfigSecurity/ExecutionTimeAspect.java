ab77763d8335516342f681c04765237acf476020
Priyanshu-choudhary <broyadi23@gmail.com>
2024-07-02 06:09:26 +0530

    Api Testing and logging

---
+++ src/main/java/com/example/WebSecurityExample/ConfigSecurity/ExecutionTimeAspect.java
@@ -0,0 +1,24 @@
+package com.example.WebSecurityExample.ConfigSecurity;
+
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.stereotype.Component;
+
+@Aspect
+@Component
+public class ExecutionTimeAspect {
+
+    @Around("execution(* com.example.WebSecurityExample..*(..))") // Adjust the package to your application package
+    public Object logExecutionTime(ProceedingJoinPoint joinPoint) throws Throwable {
+        long startTime = System.currentTimeMillis();
+
+        Object proceed = joinPoint.proceed();
+
+        long endTime = System.currentTimeMillis();
+        long executionTime = endTime - startTime;
+
+        System.out.println(joinPoint.getSignature() + " executed in " + executionTime + "ms");
+        return proceed;
+    }
+}
