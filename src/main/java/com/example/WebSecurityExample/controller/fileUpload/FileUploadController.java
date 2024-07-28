47df960dd5e0c962efc87b99282f4d449455fc89
Priyanshu-choudhary <broyadi23@gmail.com>
2024-07-28 18:55:34 +0530

    file upload

--- src/main/java/com/example/WebSecurityExample/controller/fileUpload/FileUploadController.java
+++ src/main/java/com/example/WebSecurityExample/controller/fileUpload/FileUploadController.java
@@ -1,10 +1,7 @@
 package com.example.WebSecurityExample.controller.fileUpload;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.File;
@@ -14,6 +11,7 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 @RestController
 @RequestMapping("/Files")
+@CrossOrigin(origins = {"https://codeforchallenge.online", "http://localhost:5173"})
 public class FileUploadController {
 
         @Value("${upload.dir}")
